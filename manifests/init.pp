dcbadb69dd0a801db1702eca7287a24a981e281e
rds13 <rds@panther.tique.grp>
2014-01-02 11:37:07 +0100

    allow removal of package

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,24 @@
 # Examples
 #
 #   include firefox
-class firefox($locale = 'en-US', $version = '26.0'){
+class firefox($locale = 'en-US', $version = '26.0', $ensure = present){
+  case $ensure {
+    present: {
+      Package {
+        ensure => present
+      }
+    }
+
+    absent: {
+      Package {
+        ensure => absent
+      }
+    }
+
+    default: {
+      fail("Ensure must be present or absent!")
+    }
+  }
   package { 'Firefox':
     source   => "http://ftp.mozilla.org/pub/mozilla.org/firefox/releases/${version}/mac/${locale}/Firefox%20${version}.dmg",
     provider => 'appdmg'
